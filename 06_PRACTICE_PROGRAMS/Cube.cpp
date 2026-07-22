bacd0abc454a15fea2c5f790d684dc7eb15dcafd
Vinit <vinit18914119v@gmail.com>
2026-07-22 21:58:57 +0530

    Cube's checking program.
    Today I write cube's program, by which user enter a value and the cubes will print from 1 to n.

---
+++ 06_PRACTICE_PROGRAMS/Cube.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for( int i = 1; i <= n; i++ )
+    {
+        cout<<i<<"'s cube = "<<i*i*i<<endl;
+    }
+    return 0;
+}
